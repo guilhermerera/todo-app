5a46ecf9c407b9d26512dc111bf6129963a1ce1a
guilhermerera <85325067+guilhermerera@users.noreply.github.com>
2022-10-19 17:46:52 -0300

    responsive size fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import style from "./Header.module.css";
 export default function Header() {
 	return (
 		<header className={style.container}>
-			<img src={Logo} />
+			<img className={style.logo} src={Logo} />
 		</header>
 	);
 }
